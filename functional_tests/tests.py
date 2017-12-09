0699ed6409e7232f7f0f5f9904ff802f001d7d7e
Bekzat Shayakhmetov <smbekzat@hotmail.com>
2017-12-09 23:50:38 +0100

    staging site arg fixed

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-import sys
+import os
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -8,24 +8,12 @@ from selenium import webdriver
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        for argv in sys.argv:
-            if 'liveserver' in argv:
-                cls.server_url = "http" + argv.spplit("=")[-1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
@@ -38,7 +26,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To Do List', self.browser.title)
@@ -61,7 +49,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # see the prev user's data (sessions)
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         body = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn("1: A new list", body)
 
@@ -79,7 +67,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('A new list', page_text)
 
     def test_styling_and_layout(self):
-        self.browser.get(self.server_url)
+        self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
         input_box = self.browser.find_element_by_id("input_text")
