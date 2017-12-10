f3b5d64a0b6517b8ede9c5f28ef3009d8de9b551
Bekzat Shayakhmetov <smbekzat@hotmail.com>
2017-12-10 14:53:36 +0100

    fabfile fixed + settings modified

--- deplyment_tools/fabfile.py
+++ deplyment_tools/fabfile.py
@@ -6,12 +6,12 @@ REPO_URL = 'https://github.com/smbkzt/smbkzt.tk.git'
 
 
 def deploy():
-    site_folder = '/home/smbkzt/sites/104.131.115.175'
-    run('mkdir -p 104.131.115.175')
+
+    site_folder = '/home/smbkzt/sites/{0}'.format(env.host)
+    run('mkdir -p {0}'.format(site_folder))
     with cd(site_folder):
         _get_latest_source()
         _update_settings(env.host)
-        _update_virtualenv()
         _update_static_files()
         _update_database()
 
@@ -20,9 +20,9 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run('git clone https://github.com/smbkzt/smbkzt.tk.git')
+        run('git clone {0} .'.format(REPO_URL))
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    run('git reset --hard {current_commit}')
+    run('git reset --hard {0}'.format(current_commit))
 
 
 def _update_settings(site_name):
@@ -30,19 +30,27 @@ def _update_settings(site_name):
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["{site_name}"]'
+        'ALLOWED_HOSTS = ["{0}"]'.format(site_name)
         )
+
+    secret_key_file = 'first_tdd/secret_key.py'
+    if not exists(secret_key_file):
+        key = ''.join([random.SystemRandom().choice(
+            'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+            ) for i in range(50)])
+        append(secret_key_file, 'SECRET_KEY = "{0}"'.format(key))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):
-        run('python3 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+    if not exists('tddenv/bin/pip'):
+        run('python3 -m venv tddenv')
+    run('./tddenv/bin/pip install -r requirements.txt')
 
 
 def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+    run('./tddenv/bin/python3 manage.py collectstatic --noinput')
 
 
 def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+    run('./tddenv/bin/python3 manage.py migrate --noinput')
